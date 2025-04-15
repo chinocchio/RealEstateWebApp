099d7b820ce3883856260812c7bd662559183c37
chinocchio <77035738+chinocchio@users.noreply.github.com>
2025-04-15 10:21:39 +0800

    Added Property Controller

--- RealEstateWebApp/Controllers/HomeController.cs
+++ RealEstateWebApp/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-using System.Data;
-using System.Data.OleDb;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
 using RealEstateWebApp.Models;
 
 namespace RealEstateWebApp.Controllers
@@ -10,83 +7,16 @@ namespace RealEstateWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private IWebHostEnvironment Environment;
-        private IConfiguration Configuration;
 
-        public HomeController(ILogger<HomeController> logger,
-                              IWebHostEnvironment _webHostEnvironment,
-                              IConfiguration _configuration)
+
+        public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            this.Environment = _webHostEnvironment;
-            this.Configuration = _configuration;
+
         }
 
-        public IActionResult Index(IFormFile postedFile)
+        public IActionResult Index()
         {
-            if (postedFile != null)
-            {
-                //Create a Folder.
-                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-
-                //Save the uploaded Excel file.
-                string fileName = Path.GetFileName(postedFile.FileName);
-                string filePath = Path.Combine(path, fileName);
-                using (FileStream stream = new FileStream(filePath, FileMode.Create))
-                {
-                    postedFile.CopyTo(stream);
-                }
-
-                //Read the connection string for the Excel file.
-                string constr = this.Configuration.GetConnectionString("ExcelConString");
-                DataTable dt = new DataTable();
-                constr = string.Format(constr, filePath);
-
-                using (OleDbConnection connExcel = new OleDbConnection(constr))
-                {
-                    using (OleDbCommand cmdExcel = new OleDbCommand())
-                    {
-                        using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
-                        {
-                            cmdExcel.Connection = connExcel;
-
-                            //Get the name of First Sheet.
-                            connExcel.Open();
-                            DataTable dtExcelSchema;
-                            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
-                            string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
-                            connExcel.Close();
-
-                            //Read Data from First Sheet.
-                            connExcel.Open();
-                            cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
-                            odaExcel.SelectCommand = cmdExcel;
-                            odaExcel.Fill(dt);
-                            connExcel.Close();
-                        }
-                    }
-                }
-
-                //Insert the Data read from the Excel file to Database Table.
-                constr = this.Configuration.GetConnectionString("DefaultConnectionString");
-                using (SqlConnection con = new SqlConnection(constr))
-                {
-                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
-                    {
-                        //Set the database table name.
-                        sqlBulkCopy.DestinationTableName = "dbo.Properties";
-
-                        con.Open();
-                        sqlBulkCopy.WriteToServer(dt);
-                        con.Close();
-                    }
-                }
-            }
-
             return View();
         }
 
---
+++ RealEstateWebApp/Controllers/PropertyController.cs
@@ -0,0 +1,87 @@
+﻿using System.Data.OleDb;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace RealEstateWebApp.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private IWebHostEnvironment Environment;
+        private IConfiguration Configuration;
+
+        public PropertyController(IWebHostEnvironment _webHostEnvironment, IConfiguration _configuration)
+        {
+            this.Environment = _webHostEnvironment;
+            this.Configuration = _configuration;
+        }
+
+        public IActionResult Index(IFormFile postedFile)
+        {
+            if (postedFile != null)
+            {
+                //Create a Folder.
+                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                //Save the uploaded Excel file.
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string filePath = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+
+                //Read the connection string for the Excel file.
+                string constr = this.Configuration.GetConnectionString("ExcelConString");
+                DataTable dt = new DataTable();
+                constr = string.Format(constr, filePath);
+
+                using (OleDbConnection connExcel = new OleDbConnection(constr))
+                {
+                    using (OleDbCommand cmdExcel = new OleDbCommand())
+                    {
+                        using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                        {
+                            cmdExcel.Connection = connExcel;
+
+                            //Get the name of First Sheet.
+                            connExcel.Open();
+                            DataTable dtExcelSchema;
+                            dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                            string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                            connExcel.Close();
+
+                            //Read Data from First Sheet.
+                            connExcel.Open();
+                            cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                            odaExcel.SelectCommand = cmdExcel;
+                            odaExcel.Fill(dt);
+                            connExcel.Close();
+                        }
+                    }
+                }
+
+                //Insert the Data read from the Excel file to Database Table.
+                constr = this.Configuration.GetConnectionString("DefaultConnectionString");
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                    {
+                        //Set the database table name.
+                        sqlBulkCopy.DestinationTableName = "dbo.Properties";
+
+                        con.Open();
+                        sqlBulkCopy.WriteToServer(dt);
+                        con.Close();
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
